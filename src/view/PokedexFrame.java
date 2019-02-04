cd100884b3289a2a1183f1ac8270d0beb4a7716d
Rylan Metcalfe <robotdjman12@gmail.com>
2019-02-04 13:21:06 -0700

    Basic framework for PokedexFrame and PokedexPanel

--- src/view/PokedexFrame.java
+++ src/view/PokedexFrame.java
@@ -1,11 +1,28 @@
 package view;
 
 import controller.PokedexController;
+import javax.swing.JFrame;
 
-public class PokedexFrame
+
+public class PokedexFrame extends JFrame
 {
+	private PokedexPanel pokePanel;
+	private PokedexController pokeController;
+	
 	public PokedexFrame(PokedexController app)
 	{
-		PokedexPanel panel = new PokedexPanel();
+		super();
+		this.pokeController = app;
+		pokePanel = new PokedexPanel(app);
+		
+		setupFrame();
+	}
+	public void setupFrame()
+	{
+		this.setContentPane(pokePanel);
+		this.setTitle("Pokedex");
+		this.setSize(800, 600);
+		this.setResizable(false);
+		this.setVisible(true);
 	}
 }
--- src/view/PokedexPanel.java
+++ src/view/PokedexPanel.java
@@ -1,6 +1,41 @@
 package view;
 
-public class PokedexPanel
-{
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.*;
+import controller.PokedexController;
 
+public class PokedexPanel extends JPanel
+{
+	private PokedexController pokeController;
+	private SpringLayout appLayout;
+	
+	public PokedexPanel(PokedexController appController)
+	{
+		super();
+		
+		appLayout = new SpringLayout();
+		this.pokeController = appController;
+		
+		setupPanel();
+		setuplayout();
+		setupListeners();
+	}
+	private void setuplayout()
+	{
+
+	}
+	private void setupListeners() 
+	{
+		
+	}
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setBackground(Color.LIGHT_GRAY);
+		this.setPreferredSize(new Dimension(800, 600));
+	}
 }
